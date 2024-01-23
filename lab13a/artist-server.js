d2965ca53b771618110e5a86c6337de59fa4debe
Kyle Duong <kyleduong13@gmail.com>
2024-01-22 21:29:52 -0700

    Completed TYK1

--- lab13a/artist-server.js
+++ lab13a/artist-server.js
@@ -1,27 +1,62 @@
-const url = require("url");
-const http = require("http");
 const path = require("path");
+const express = require('express');
 const fs = require("fs");
+const jsonPath = path.join(__dirname, 'data/artists.json')
+const jsonData = fs.readFileSync(jsonPath, 'utf-8')
+const artists = JSON.parse(jsonData);
 
-const server = http.createServer(async (req, res) => {
-    const fileName = path.join(__dirname, 'data/artists.json')
-    console.log(fileName);
+const app = express();
 
-    try {
-        const cont = await fs.readFileSync(fileName);
-        res.writeHead(200, {"Content-type": "application/json"});
-        res.write(cont)
-    } catch {
-        res.json({message: "Error"})
-    }
+app.use('/api/artists/name/:value', (req, res) => {
+    const lNameSearch = req.params.value.toLowerCase();
+
+    // search the array of objects for a match 
+    const matches = artists.filter(a => lNameSearch === a.LastName.substring(0, lNameSearch.length).toLowerCase());       
     
-    res.end();
+    // return the matching stock 
+    if (matches.length > 0) { 
+        res.json(matches);    
+    } else {      
+        res.json({message: `${lNameSearch} not found`});    
+    }
 })
 
-const port = 8080;
-server.listen(port);
+app.use('/api/artists/nationality/:value', (req, res) => {
+    const nationalitySearch = req.params.value.toLowerCase();
 
-// display a message on the terminal
-console.log("Server running at port=" + port);
-console.log("dirname = " + __dirname);
-console.log("filename = " + __filename);
+    // search the array of objects for a match 
+    const matches = artists.filter(a => nationalitySearch === a.Nationality.toLowerCase());       
+    
+    // return the matching stock 
+    if (matches.length > 0) { 
+        res.json(matches);    
+    } else {      
+        res.json({message: `Nationality ${nationalitySearch} not found`});    
+    }
+})
+
+app.use('/api/artists/:id', (req, res) => {
+    const idSearch = req.params.id;
+
+    // search the array of objects for a match 
+    const matches = artists.filter(a => idSearch === a.ArtistID);       
+    
+    // return the matching stock 
+    if (matches.length > 0) { 
+        res.json(matches);    
+    } else {       
+        res.json({message: `ID ${idSearch} not found`});  
+    }
+})
+
+
+// handle all requests
+app.use('/', (req, res) => {
+    res.json(artists);
+})
+
+ // Use express to listen to port 
+ let port = 8080; 
+ app.listen(port, () => { 
+     console.log("Server running at port= " + port); 
+ }); 

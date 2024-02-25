26deec591e498361c3ab440bdc75d1165bca08a1
Kyle Duong <kyleduong13@gmail.com>
2024-02-25 13:58:58 -0700

    Completed 14b

--- lab14b/f1-server.js
+++ lab14b/f1-server.js
@@ -42,9 +42,57 @@ app.get('/f1/results/:race', async (req, res) => {
     res.send(data);
 });
 
+app.get('/f1/qualifying/:race1', async (req, res) => {
+    const {data, err} = await supabase
+        .from('qualifying')
+        .select(`
+            qualifyId, position, q1, q2, q3, races(name, year), drivers(surname, forename), constructors(name)
+        `)
+        .eq('raceId', req.params.race1)
+        .order('position', {ascending: true});
+
+    res.send(data);
+});
+
+app.get('/f1/races/:year1/:year2', async (req, res) => {
+    const {data, error} = await supabase
+        .from('races')
+        .select()
+        .gte("year", req.params.year1)
+        .lte("year", req.params.year2)
+        .order('year', { ascending: true });
+    res.send(data);
+});
+
+app.get('/f1/races/:year1/:year2', async (req, res) => {
+    const {data, error} = await supabase
+        .from('races')
+        .select()
+        .gte("year", req.params.year1)
+        .lte("year", req.params.year2)
+        .order('year', { ascending: true });
+    res.send(data);
+});
+
+app.get('/f1/drivers/name/:name/limit/:limit', async (req, res) => {
+    const {data, error} = await supabase
+        .from('drivers')
+        .select()
+        .ilike("surname", `${req.params.name}%`)
+        .limit(req.params.limit)
+        .order("surname", {ascending: true});
+
+    res.send(data);
+});
+
+
+
 app.listen(8080, () => {
     console.log('listening on port 8080');
-    console.log('http://localhost:8080/f1/status');
-    console.log('http://localhost:8080/f1/seasons');
-    console.log('http://localhost:8080/f1/races');
+    // console.log('http://localhost:8080/f1/status');
+    // console.log('http://localhost:8080/f1/seasons');
+    // console.log('http://localhost:8080/f1/races');
+    console.log('http://localhost:8080/f1/qualifying/1034');
+    console.log('http://localhost:8080/f1/races/2020/2022');
+    console.log('http://localhost:8080/f1/drivers/name/sch/limit/12');
 });

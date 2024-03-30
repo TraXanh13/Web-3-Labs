3bad9545a100fd84e570a98ad47a3d5228874332
Kyle Duong <kyleduong13@gmail.com>
2024-03-30 00:04:21 -0600

    Completed 11c.2

---
+++ lab11c/lab11c-styling-app/src/App.jsx
@@ -0,0 +1,66 @@
+import './App.css';
+import HomeStyles from './components/HomeStyles.jsx';
+import HomeAntd from './components/HomeAntd.jsx';
+import HomeRechart from './components/HomeRechart.jsx';
+import Navigation from './components/Navigation.jsx';
+import { Routes, Route } from 'react-router-dom';
+
+const App = (props) => {
+
+
+  return (
+    <div>
+      <header>
+        <h2>Lab11c - React 3</h2>
+        <Navigation />
+      </header>
+      <main>
+      <Routes>
+        <Route path='/styles' element={<HomeStyles />} />
+        <Route path='/antd' element={<HomeAntd />} />
+        <Route path='/recharts' element={<HomeRechart />} />
+      </Routes>        
+      </main>  
+    </div>
+  );
+
+};
+
+export default App;
+
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
